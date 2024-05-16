ff9874b61c8c5a69daea3b276b5ce33a81c0c728
waheb-benzaid <waheb.benzaid@gmail.com>
2024-05-16 20:02:56 +0100

    Add project files.

---
+++ AlgorithmRefresher/PolindromWord.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgorithmRefresher
+{
+    public class PolindromeWord(string word)
+    {
+        private readonly string _words = word;
+
+        private bool isPolindrome()
+        {
+            return false;
+        }
+    }
+}
