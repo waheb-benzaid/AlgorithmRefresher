8f39570ba806cdc2e0977365973720db0e74728a
waheb-benzaid <waheb.benzaid@gmail.com>
2024-05-17 23:12:48 +0100

    solving polindrom word problem

--- AlgorithmRefresher/PolindromWord.cs
+++ AlgorithmRefresher/PolindromWord.cs
@@ -1,12 +1,36 @@
 ﻿namespace AlgorithmRefresher
 {
-    public class PolindromeWord(string word)
+    public class PolyndromeWord(string word)
     {
-        private readonly string _words = word;
-
-        private bool isPolindrome()
+        public bool isPolindromeMySolution()
         {
-            return false;
+            int length = word.Length;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = length - 1; j > 0; j--)
+                {
+                    if (word[i] != word[j])
+                    {
+                        return false;
+                    }
+                    i++;
+                }
+            }
+            return true;
+        }
+        //****************************************************
+        public void PolindromeBetterSolution()
+        {
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (word[i] != word[word.Length - i - 1])
+                {
+                    Console.WriteLine("False");
+                    break;
+                }
+                break;
+            }
+            Console.WriteLine("true");
         }
     }
 }
--- AlgorithmRefresher/Program.cs
+++ AlgorithmRefresher/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AlgorithmRefresher;
+
+var Polyndrom = new PolyndromeWord("pkpkpkp");
+
+Polyndrom.PolindromeBetterSolution();
